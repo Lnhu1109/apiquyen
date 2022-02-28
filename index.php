22504a35d021aa6c035598c40b90cc8015ab8ebc
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-02-28 17:27:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Api của Lê Công Quyền</title>
+</head>
+<body>
+<h3>Welcome to my web api</h1>
+<h3>"Api gái" : https://www.congquyen-api.tk/gai.php</h3>
+<h3>"Api gái sexy" : https://www.congquyen-api.tk/gaisexy.php</h3>
+<h3>"Api vú gái" : https://www.congquyen-api.tk/vú.php</h3>
+<h3>"Api mông gái" : https://www.congquyen-api.tk/mông.php</h3>
+<h3>"Api thính" : https://www.congquyen-api.tk/thính.php</h3>
+<h3>"Api cadao" : https://www.congquyen-api.tk/cadao.php"</h3>
+<h3>"Api punch anime" : https://www.congquyen-api.tk/đấm.php</h3>
+<h3>"Api slap anime" : https://www.congquyen-api.tk/tát.php</h3>
+<h3>"Api spar anime" : https://www.congquyen-api.tk/đá.php</h3>
+<h3>"Api kiss anime" : https://www.congquyen-api.tk/hôn.php</h3>
+<h3>"Api video gái" : https://www.congquyen-api.tk/vdgai.php</h3>
+</body>
+</html>
---
+++ đấm.php
@@ -0,0 +1,34 @@
+<?php
+function getRandomFromArray($ar) {
+mt_srand( (double)microtime() * 1000000 );
+$num = array_rand($ar);
+return $ar[$num];
+}
+
+function getImagesFromDir($path) {
+$images = array();
+if ( $img_dir = @opendir($path) ) {
+while ( false !== ($img_file = readdir($img_dir)) ) {
+// checks for gif, jpg, png
+if ( preg_match("/(\.gif|\.jpg|\.mp4|\.png|\.jpeg)$/", $img_file) ) {
+$images[] = $img_file;
+}
+}
+closedir($img_dir);
+}
+return $images;
+}
+
+$root = '';
+$path = 'api/punch/';
+$imgList = getImagesFromDir($root . $path);
+$img = getRandomFromArray($imgList);
+?>
+<?php
+$pipi = array(
+"data" => "https://www.congquyen-api.tk/api/punch/".$img,
+"author" => "Lê Công Quyền",
+);
+$json = json_encode($pipi, JSON_UNESCAPED_SLASHES);
+print($json);
+?>
