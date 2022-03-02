10b4f3190119b3cc3dced1a7de568f9ac3b4f8ad
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-03-02 08:24:45 +0700

    Update gai.php

--- gai.php
+++ gai.php
@@ -26,7 +26,7 @@ $img = getRandomFromArray($imgList);
 ?>
 <?php
 $pipi = array(
-"data" => "https://www.congquyen-api.tk/api/gai/".$img,
+"data" => "https://imgapicongquyen.herokuapp.com/api/gai/".$img,
 );
 $json = json_encode($pipi, JSON_UNESCAPED_SLASHES);
 print($json);
