0081a8a6c62e499d458c1ee8f5ca2c91af0c4a81
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-03-02 08:28:55 +0700

    Update mông.php

--- mông.php
+++ mông.php
@@ -26,7 +26,7 @@ $img = getRandomFromArray($imgList);
 ?>
 <?php
 $pipi = array(
-"data" => "https://www.congquyen-api.tk/api/gaiditbu/".$img,
+"data" => "https://imgapicongquyen.herokuapp.com/api/gaiditbu/".$img,
 );
 $json = json_encode($pipi, JSON_UNESCAPED_SLASHES);
 print($json);
