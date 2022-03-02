fec2080eb6b2b37b94e6b0b517ad5d3dcdaa2772
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-03-02 08:26:17 +0700

    Update hôn.php

--- hôn.php
+++ hôn.php
@@ -26,7 +26,7 @@ $img = getRandomFromArray($imgList);
 ?>
 <?php
 $pipi = array(
-"data" => "https://www.congquyen-api.tk/api/kiss/".$img,
+"data" => "https://imgapicongquyen.herokuapp.com/api/kiss/".$img,
 "author" => "Lê Công Quyền",
 );
 $json = json_encode($pipi, JSON_UNESCAPED_SLASHES);
