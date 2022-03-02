f088de2a749b4d56806e1efe13e9577048ef255b
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-03-02 08:30:35 +0700

    Update đấm.php

--- đấm.php
+++ đấm.php
@@ -26,7 +26,7 @@ $img = getRandomFromArray($imgList);
 ?>
 <?php
 $pipi = array(
-"data" => "https://www.congquyen-api.tk/api/punch/".$img,
+"data" => "https://imgapicongquyen.herokuapp.com/api/punch/".$img,
 "author" => "Lê Công Quyền",
 );
 $json = json_encode($pipi, JSON_UNESCAPED_SLASHES);
